64a9967a6f54ff0d6b2ff9d1bc90fb820037d2d7
Sara-Barbi <barbisara02@gmail.com>
2021-12-13 13:10:30 +0100

    ricerco elemento dopo for

--- myscript.js
+++ myscript.js
@@ -1,1 +1,13 @@
-const email = ["sarabarbi02@gmail.com","mionome@miosito.it", "mario.tecca@ilsito.org", "mario@il.miosito.it" ]
+const myEmail = ["sarabarbi02@gmail.com","mionome@miosito.it", "mario.tecca@ilsito.org", "mario@il.miosito.it"];
+
+const cerca = prompt("inserisci la tua email.");
+
+let controlloEm = false;
+
+for(let i = 0, i = myEmail.length, i++){
+    if(cerca == myEmail[i]){
+        controlloEm = true "email verificata!"
+    }
+}
+
+document.getElementById("elem").innerHTML = controlloEm
