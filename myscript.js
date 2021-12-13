6926bc664485620a01a4409bb79a06b7eac1ce84
Sara-Barbi <barbisara02@gmail.com>
2021-12-13 13:59:54 +0100

    numeri random

--- myscript.js
+++ myscript.js
@@ -4,13 +4,27 @@ const cerca = prompt("inserisci la tua email.");
 
 let controlloEm = false;
 
-for(let index = 0, index = myEmail.length ,index++){
+for(let index = 0 ; index < myEmail.length ;index++){
     if(cerca == myEmail[index]){
-        controlloEm = true "email verificata!";
+        controlloEm = true;
     }
 }
+if(controlloEm){
+    document .getElementById("elem").innerHTML = "email verificata";
+}else{
+    document .getElementById("elem").innerHTML = "email NON verificata";
+}
 
-document .getElementById("elem").innerHTML = controlloEm;
 
+//Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
+let utente1 = (math.floormath.random()*5+1);
+let computer = (math.floormath.random()*5+1);
+
+if(utente1 > computer){
+    document .getElementById("numeri").innerHTML = "utente1 vincitore";
+    
+}else{
+    document .getElementById("numeri").innerHTML = "utente2 vincitore";
+}
 
