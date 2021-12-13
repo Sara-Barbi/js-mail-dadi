d3dbd0d1bc44a3a0c31232d613b7b61d1a1eb121
Sara-Barbi <barbisara02@gmail.com>
2021-12-13 15:53:44 +0100

    2folders

--- js1/myscript.js
+++ js1/myscript.js
@@ -16,15 +16,9 @@ if(controlloEm){
 }
 
 
-//Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
-let utente1 = (math.floormath.random()*5+1);
-let computer = (math.floormath.random()*5+1);
 
-if(utente1 > computer){
-    document .getElementById("numeri").innerHTML = "utente1 vincitore";
-    
-}else{
-    document .getElementById("numeri").innerHTML = "utente2 vincitore";
-}
+
+
+
 
---
+++ js2/mysecondscript.js
@@ -0,0 +1,14 @@
+//Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+let utente1 = (math.floormath.random()*5+1);
+let computer = (math.floormath.random()*5+1);
+document.getElementById("numeri")= utente1 + computer;
+if(utente1 > computer){
+   
+    document .getElementById("numeri").innerHTML = "utente1 vincitore";
+
+}else{
+
+    document .getElementById("numeri").innerHTML = "utente2 vincitore";
+
+}
